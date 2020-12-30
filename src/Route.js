74786d4392e048977ad3987cc2ba7e14419df54d
ahmedmalick <ahmedmalik2k20@gmail.com>
2020-12-31 00:11:37 +0500

    added and updated some files and folder

--- src/Route.js
+++ src/Route.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './components/Home';
-import Product from './components/Product';
-import ProductItems from './components/ProductItems';
-import AboutUs from './components/AboutUs';
-import ContactUs from './components/ContactUs';
+import Home from './components/Home/Home';
+import Product from './components/Product/Product';
+import ProductItems from './components/ProductItems/ProductItems';
+import AboutUs from './components/AboutUs/AboutUs';
+import ContactUs from './components/ContactUs/ContactUs';
 
 function RouteConfig() {
     return (
         <div>
             <Router>
                 <Routes>
-                    <Route path="/" component={<Home />} />
-                    <Route path="product" component={<Product />} />
-                    <Route path="productitems" component={<ProductItems />} />
-                    <Route path="aboutus" component={<AboutUs />} />
-                    <Route path="contactus" component={<ContactUs />} />
+                    <Route path="/" element={<Home />} />
+                    <Route path="product" element={<Product />} />
+                    <Route path="productitems/:id" element={<ProductItems />} />
+                    <Route path="aboutus" element={<AboutUs />} />
+                    <Route path="contactus" element={<ContactUs />} />
                 </Routes>
             </Router>
         </div>
