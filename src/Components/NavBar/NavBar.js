c87ca5662a77d33b386263593b667e3e0382d4ae
ahmedmalick <ahmedmalik2k20@gmail.com>
2021-06-08 13:37:02 +0500

    finally done Shoe Store App

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,27 +1,38 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { FaBars } from 'react-icons/fa';
 import logo from '../../Assests/logo.png'
 import cart from '../../Assests/cart.png'
+import menu from '../../Assests/menu.png'
 import styles from './NavBar.module.css'
 
 const NavBar = () => {
+
+    let [menuIcon, setMenuIcon] = useState(false);
+
     return (
         <div className={styles.navbar}>
-            <div className={styles.logo}>
+            <div>
                 <Link to='/'>
                     <img src={logo} alt='RedStore' width='125px' />
                 </Link>
             </div>
             <nav>
-                <ul id='menuItems'>
+                <ul style={{
+                    maxHeight: menuIcon ? '200px' : '0px'
+                }}>
                     <li><Link to='/'>Home</Link></li>
                     <li><Link to='/products'>Products</Link></li>
                     <li><Link to='/aboutus'>About Us</Link></li>
                     <li><Link to='/contactus'>Contact Us</Link></li>
                 </ul>
             </nav>
-            <FaBars className={styles.menu_icon} />
             <img src={cart} alt='Cart' width='30px' height='30px' />
+            <img
+                className={styles.menu_icon}
+                src={menu}
+                alt='Menu Icon'
+                onClick={() => setMenuIcon(!menuIcon)}
+            />
         </div>
     )
 }
